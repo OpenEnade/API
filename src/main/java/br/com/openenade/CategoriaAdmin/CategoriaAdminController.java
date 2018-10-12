de4c5f7562cb8075997a7bc03068fec126683608
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2018-10-12 17:02:08 -0300

    fixed a minor error

--- src/main/java/br/com/openenade/CategoriaAdmin/CategoriaAdminController.java
+++ src/main/java/br/com/openenade/CategoriaAdmin/CategoriaAdminController.java
@@ -1,6 +1,7 @@
 package br.com.openenade.CategoriaAdmin;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class CategoriaAdminController {
     
     public static final String ENDPOINT = "/CategoriaAdmin";
     
+    @GetMapping
     public CategoriaAdmin[] getCategoriaAdmin() {
         
         return CategoriaAdmin.values();
