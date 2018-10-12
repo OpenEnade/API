f50d74c99289b3805f76162e5e083a239260675a
icarolima <icarolima@lsd.ufcg.edu.br>
2018-10-12 01:24:10 -0300

    Added @ResponseBody on ModalidadeController

--- src/main/java/br/com/openenade/api/modalidade/ModalidadeController.java
+++ src/main/java/br/com/openenade/api/modalidade/ModalidadeController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class ModalidadeController {
 
     
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<Modalidade[]> index() {
+    public @ResponseBody ResponseEntity<Modalidade[]> index() {
         return  new ResponseEntity<>(Modalidade.values(), HttpStatus.OK);
     }
 
