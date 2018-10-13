91c5dbb815cbe9ecc5ec4f75a29ae9b344d792c8
icarolima <icarolima@lsd.ufcg.edu.br>
2018-10-13 18:24:12 -0300

    Refact

--- src/main/java/br/com/openenade/api/curso/CursoController.java
+++ src/main/java/br/com/openenade/api/curso/CursoController.java
@@ -1,11 +1,11 @@
 package br.com.openenade.api.curso;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/br/com/openenade/api/curso/CursoService.java
+++ src/main/java/br/com/openenade/api/curso/CursoService.java
@@ -1,7 +1,7 @@
 package br.com.openenade.api.curso;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class CursoService {
