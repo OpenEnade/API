b0d5e4be751ee89d5b1e1d805218028078e0d630
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-10-17 06:39:56 -0300

    Implements Curso equals and hashcode

--- src/main/java/br/com/openenade/api/curso/Curso.java
+++ src/main/java/br/com/openenade/api/curso/Curso.java
@@ -65,6 +65,31 @@ public class Curso {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigoCurso == null) ? 0 : codigoCurso.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Curso other = (Curso) obj;
+        if (codigoCurso == null) {
+            if (other.codigoCurso != null)
+                return false;
+        } else if (!codigoCurso.equals(other.codigoCurso))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "Curso [codigoCurso=" + codigoCurso + ", nome=" + nome + ", codigoArea=" + codigoArea
                 + ", modalidade=" + modalidade + "]";
