a345e843e1168611a41b5c5d0560d83b08db81f9
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2018-10-18 23:23:11 -0300

    Regiao task fix

--- src/test/java/br/com/openenade/api/regiao/RegiaoServiceTests.java
+++ src/test/java/br/com/openenade/api/regiao/RegiaoServiceTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.util.List;
-import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
