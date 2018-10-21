9931ec697e64d890f175b29016d0255b571297ef
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-10-20 22:53:59 -0300

    Fix Curso enum javax annotation

--- src/main/java/br/com/openenade/api/curso/Curso.java
+++ src/main/java/br/com/openenade/api/curso/Curso.java
@@ -2,6 +2,8 @@ package br.com.openenade.api.curso;
 
 import javax.persistence.Id;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.NotBlank;
@@ -21,6 +23,7 @@ public class Curso {
     @Positive(message = "'codigoArea' precisa ser positivo.")
     private Long codigoArea;
 
+    @Enumerated(EnumType.STRING)
     @NotNull(message = "Você precisa especificar uma 'modalidade' de ensino.")
     private Modalidade modalidade;
 
