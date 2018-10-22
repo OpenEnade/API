1f978edbca1c08876d8130e7d56797af2bb050e7
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2018-10-22 19:03:49 -0300

    fixing build errors

--- src/main/java/br/com/openenade/api/estado/Estado.java
+++ src/main/java/br/com/openenade/api/estado/Estado.java
@@ -4,7 +4,6 @@ package br.com.openenade.api.estado;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import br.com.openenade.api.regiao.Regiao;
