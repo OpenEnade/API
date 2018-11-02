68a8b9fb1ff73c56725e3cb1be7f96f88f0639fc
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2018-11-02 17:32:44 -0300

    Added the resource of Universidade (Entity, Repository, Controller and Service)

--- src/main/java/br/com/openenade/api/universidade/Universidade.java
+++ src/main/java/br/com/openenade/api/universidade/Universidade.java
@@ -100,6 +100,7 @@ public class Universidade {
                 + ", categoriaAdmin=" + categoriaAdmin + ", curso=" + curso + "]";
     }
 
+    
     @Override
     public int hashCode() {
         final int prime = 31;
