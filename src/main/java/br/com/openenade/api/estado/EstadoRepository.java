12eb6a3a039fbe90c989fa68de1fe75eed6b6fc3
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-11-05 17:15:20 -0300

    Fix estado and municipio tests applying delete in cascade

--- src/main/java/br/com/openenade/api/estado/EstadoRepository.java
+++ src/main/java/br/com/openenade/api/estado/EstadoRepository.java
@@ -1,11 +1,10 @@
 package br.com.openenade.api.estado;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, String> {
 
-    public List<Estado> findEstadosByRegiaoSigla(String regiaoSigla);
+    public void deleteEstadosByRegiaoSigla(String regiaoSigla);
 }
