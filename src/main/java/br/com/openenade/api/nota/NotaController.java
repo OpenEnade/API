06b439a5643bc4e7c26355b6379e46216ebf47f5
icarolima <icarolima@lsd.ufcg.edu.br>
2018-11-08 22:30:37 -0300

    Implemented save on Nota Service and Controller

--- src/main/java/br/com/openenade/api/nota/NotaController.java
+++ src/main/java/br/com/openenade/api/nota/NotaController.java
@@ -1,11 +1,14 @@
 package br.com.openenade.api.nota;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,11 @@ public class NotaController {
 
     @Autowired
     private NotaService service;
+
+    @PostMapping
+    public void postNota(@Valid @RequestBody Nota nota) {
+        this.service.save(nota);
+    }
 
     @GetMapping
     @ResponseBody
--- src/main/java/br/com/openenade/api/nota/NotaService.java
+++ src/main/java/br/com/openenade/api/nota/NotaService.java
@@ -1,9 +1,17 @@
 package br.com.openenade.api.nota;
 
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+import br.com.openenade.api.ano.Ano;
+import br.com.openenade.api.ano.AnoRepository;
+import br.com.openenade.api.curso.Curso;
+import br.com.openenade.api.curso.CursoRepository;
+import br.com.openenade.api.universidade.Universidade;
+import br.com.openenade.api.universidade.UniversidadeRepository;
 
 @Service
 @Transactional
@@ -11,6 +19,40 @@ public class NotaService {
 
     @Autowired
     private NotaRepository notaRepository;
+
+    @Autowired
+    private AnoRepository anoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private UniversidadeRepository universidadeRepository;
+
+    public Nota save(Nota nota) {
+        Optional<Ano> optAno = this.anoRepository.findById(nota.getAno().getAno());
+
+        Curso curso = nota.getCurso();
+        curso.setNome(null);
+        curso.setModalidade(null);
+        Optional<Curso> optCurso = this.cursoRepository.findOne(Example.of(curso));
+        Optional<Universidade> optUniversidade =
+                this.universidadeRepository.findById(nota.getUniversidade().getCodigoIES());
+
+        if (!optAno.isPresent()) {
+            this.anoRepository.save(nota.getAno());
+        }
+
+        if (!optCurso.isPresent()) {
+            this.cursoRepository.save(nota.getCurso());
+        }
+
+        if (!optUniversidade.isPresent()) {
+            this.universidadeRepository.save(nota.getUniversidade());
+        }
+
+        return this.notaRepository.save(nota);
+    }
 
     public List<Nota> getAll() {
         return this.notaRepository.findAll();
