d946cddba821b640d9ff75cd73e8840e9e9baf28
Javan Lacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-11-12 18:56:41 -0300

    remove unutilized import

--- src/main/java/br/com/openenade/api/universidade/UniversidadeController.java
+++ src/main/java/br/com/openenade/api/universidade/UniversidadeController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.openenade.api.municipio.Municipio;
-
 @RestController
 @CrossOrigin("*")
 @RequestMapping(path = UniversidadeController.ENDPOINT)
