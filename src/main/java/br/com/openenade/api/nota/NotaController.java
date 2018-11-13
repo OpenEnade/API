9b32296b7af90d4681fc6e8a2b78c909ea35b986
icarolima <icarolima@lsd.ufcg.edu.br>
2018-11-13 00:18:15 -0300

    Uncomment Nota controller

--- src/main/java/br/com/openenade/api/nota/NotaController.java
+++ src/main/java/br/com/openenade/api/nota/NotaController.java
@@ -1,17 +1,23 @@
 package br.com.openenade.api.nota;
 
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.openenade.api.ano.Ano;
+import br.com.openenade.api.curso.Curso;
+import br.com.openenade.api.universidade.Universidade;
 
 @RestController
 @CrossOrigin("*")
@@ -34,27 +40,52 @@ public class NotaController {
         return new ResponseEntity<>(this.service.getAll(), HttpStatus.OK);
     }
 
-    /*@ResponseBody
-    @GetMapping(path = "/{index}")
-    public ResponseEntity<Nota> getNotaByIndex(@PathVariable(name = "index") Integer id) {
+    @ResponseBody
+    @GetMapping(path = "/{ano}/{codigoCurso}/{codigoIES}")
+    public ResponseEntity<Nota> getNotaByIndex(@PathVariable(name = "ano") Integer ano,
+            @PathVariable(name = "codigoCurso") Long codigoCurso,
+            @PathVariable(name = "codigoIES") Long codigoIES) {
 
-        Optional<Nota> optNota = this.service.getNotaById(id);
+        Ano anoObj = new Ano();
+        anoObj.setAno(ano);
+
+        Curso curso = new Curso();
+        curso.setCodigoCurso(codigoCurso);
+
+        Universidade universidade = new Universidade();
+        universidade.setCodigoIES(codigoIES);
+
+        Optional<Nota> optNota = this.service.getNotaById(new NotaId(anoObj, curso, universidade));
 
         if (optNota.isPresent()) {
             return new ResponseEntity<>(optNota.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-    }*/
+    }
 
-    /*@DeleteMapping(path = "/{index}")
-    public ResponseEntity<String> deleteNotaByIndex(@PathVariable(name = "index") Integer index) {
-        boolean deleted = this.service.deleteNotaByIndex(index);
+
+    @DeleteMapping(path = "/{ano}/{codigoCurso}/{codigoIES}")
+    public ResponseEntity<String> deleteNotaByIndex(@PathVariable(name = "ano") Integer ano,
+            @PathVariable(name = "codigoCurso") Long codigoCurso,
+            @PathVariable(name = "codigoIES") Long codigoIES) {
+
+        Ano anoObj = new Ano();
+        anoObj.setAno(ano);
+
+        Curso curso = new Curso();
+        curso.setCodigoCurso(codigoCurso);
+
+        Universidade universidade = new Universidade();
+        universidade.setCodigoIES(codigoIES);
+
+        boolean deleted = this.service.deleteNotaById(new NotaId(anoObj, curso, universidade));
 
         if (deleted) {
             return new ResponseEntity<>("Deletado.", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Index não encontrado.", HttpStatus.NOT_FOUND);
         }
-    }*/
+    }
+
 }
