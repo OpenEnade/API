67d47872aa8399a08be4e2280ba8c5d63c627f98
icarolima <icarolima@lsd.ufcg.edu.br>
2018-11-13 20:42:08 -0300

    WIP: Solved problems of NotaService

--- src/main/java/br/com/openenade/api/nota/NotaService.java
+++ src/main/java/br/com/openenade/api/nota/NotaService.java
@@ -4,15 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-import br.com.openenade.api.ano.Ano;
-import br.com.openenade.api.ano.AnoRepository;
-import br.com.openenade.api.curso.Curso;
-import br.com.openenade.api.curso.CursoRepository;
-import br.com.openenade.api.universidade.Universidade;
-import br.com.openenade.api.universidade.UniversidadeId;
-import br.com.openenade.api.universidade.UniversidadeRepository;
 
 @Service
 @Transactional
@@ -21,36 +13,7 @@ public class NotaService {
     @Autowired
     private NotaRepository notaRepository;
 
-    @Autowired
-    private AnoRepository anoRepository;
-
-    @Autowired
-    private CursoRepository cursoRepository;
-
-    @Autowired
-    private UniversidadeRepository universidadeRepository;
-
     public Nota save(Nota nota) {
-        Optional<Ano> optAno = this.anoRepository.findById(nota.getAno().getAno());
-
-        Curso curso = nota.getCurso();
-        Optional<Curso> optCurso = this.cursoRepository.findOne(Example.of(curso));
-        Optional<Universidade> optUniversidade = this.universidadeRepository
-                .findById(new UniversidadeId(nota.getUniversidade().getCodigoIES(),
-                        nota.getUniversidade().getCampus().getCodigo()));
-
-        if (!optAno.isPresent()) {
-            nota.setAno(this.anoRepository.save(nota.getAno()));
-        }
-
-        if (!optCurso.isPresent()) {
-            nota.setCurso(this.cursoRepository.save(nota.getCurso()));
-        }
-
-        if (!optUniversidade.isPresent()) {
-            nota.setUniversidade(this.universidadeRepository.save(nota.getUniversidade()));
-        }
-
         return this.notaRepository.save(nota);
     }
 
