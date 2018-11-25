930187dd4e34794a597040ae91e98e2fe02bf1b0
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-11-24 23:07:33 -0300

    Remove unnecessary files

--- build-stub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -t pfelipefeitosa/openenade-api:stub-data -f Dockerfile-stub .
--- run-stub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -idt \
-	--name openenade-api \
-	-p 8080:8080 \
-	pfelipefeitosa/openenade-api:stub-data
